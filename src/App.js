e40972e8fd7790a032ff862b1b5d8a9d65b17ee0
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-10-30 18:01:18 +0330

    create component navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-import { Card } from "react-bootstrap";
+import myNavbar from "./components/myNavbar";
 
 import "./css/style.css";
-import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
+        <myNavbar />
+        <myNavbar />
+        <myNavbar />
+        <myNavbar />
+        <myNavbar />
       </div>
     );
   }
