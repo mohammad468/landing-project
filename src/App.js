00e3caea6269bf20bf2fee4761b63857bb3ea147
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-10-30 17:35:00 +0330

    testing and start for React project landing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
-import { Button } from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 
 import "./css/style.css";
 import "./App.css";
@@ -8,7 +9,17 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Button>hello</Button>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
       </div>
     );
   }
