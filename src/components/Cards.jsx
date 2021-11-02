7542edddf505d0c6a92640cbbe6b7a508dc10a27
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-02 12:33:49 +0330

    fixed bug in images logo risponsive

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Container, Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
 import "../css/style.css";
 import MyCard from "./MyCard";
 import a12sm from "../images/assets/cards/a12sm.jpg";
@@ -23,48 +22,56 @@ class Cards extends Component {
               cost="4,000,000 T"
               name="Galaxy A12 SM-A125F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A32 SM-A325F/DS دو سیم‌کارت ظرفیت 128 گیگابایت و رم 6 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={a32sm}
               cost="7,000,000 T"
               name="Galaxy A32 SM-A325F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={iphone12pormax}
               cost="39,599,000 T"
               name="iPhone 12 Pro Max A2412"
               detail="گوشی موبایل اپل مدل iPhone 12 Pro Max A2412 دو سیم‌ کارت ظرفیت 256 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={iphone13pormax}
               cost="49,489,000 T"
               name="iPhone 13 Pro Max A2644"
               detail="گوشی موبایل اپل مدل iPhone 13 Pro Max A2644 دو سیم‌ کارت ظرفیت 256 گیگابایت و رم 6 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={iphonese}
               cost="4,000,000 T"
               name="Galaxy A12 SM-A125F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={pocof3}
               cost="4,000,000 T"
               name="Galaxy A12 SM-A125F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={pocox3}
               cost="4,000,000 T"
               name="Galaxy A12 SM-A125F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت"
+              xs={12}
             />
             <MyCard
               image={s20fe}
               cost="4,000,000 T"
               name="Galaxy A12 SM-A125F/DS"
               detail="گوشی موبایل سامسونگ مدل Galaxy A12 SM-A125F/DS دو سیم کارت ظرفیت 64 گیگابایت"
+              xs={12}
             />
           </Row>
         </Container>
--- src/components/Logoes.jsx
+++ src/components/Logoes.jsx
@@ -1,23 +1,28 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "../css/style.css";
-import { Image } from 'react-bootstrap';
+import { Col, Image, Row } from "react-bootstrap";
 import samsung from "../images/assets/Brand/samsung-Logo.jpg";
 import apple from "../images/assets/Brand/Apple-Logo.jpg";
 
-
 class Logoes extends Component {
-    render() {
-        return (
-          <div className="my-3">
-            <h2 className="text-center my-5">Who Support US ?!</h2>
-            <div className="d-flex justify-content-around my-5">
-              <Image src={apple} fluid />
-              <Image src={samsung} fluid />
-              <Image src={apple} fluid />
-            </div>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className="my-3">
+        <h2 className="text-center my-5">Who Support US ?!</h2>
+        <Row className="my-5 justify-content-center">
+          <Col xs={4} sm={3} md={3} lg={3} xl={3} xxl={3}>
+            <Image src={apple} fluid />
+          </Col>
+          <Col xs={4} sm={3} md={3} lg={3} xl={3} xxl={3}>
+            <Image src={apple} fluid />
+          </Col>
+          <Col xs={4} sm={3} md={3} lg={3} xl={3} xxl={3}>
+            <Image src={apple} fluid />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default Logoes;
