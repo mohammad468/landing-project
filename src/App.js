92eb21dd9fe1e0c4f7f64f150d539b39019c1301
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-11-06 14:47:27 +0330

    create spa project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React, { Component } from "react";
-import MyNav from "./components/MyNav";
-import Banner from "./components/Banner";
-import Cards from "./components/Cards";
-import Search from "./components/Search";
-import Logoes from "./components/Logoes";
-import Footer from "./components/Footer";
+
 
 import "./css/style.css";
 
@@ -12,12 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <MyNav/>
-        <Banner/>
-        <Cards/>
-        <Search/>
-        <Logoes/>
-        <Footer/>
+        
       </div>
     );
   }
